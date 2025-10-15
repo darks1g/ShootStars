2848dd5c549f011df2861c327c1b6863bf48f515
fsanjh <fsanjosehernan@hotmail.com>
2025-10-15 18:51:34 +0200

    refactor footer layout and styles; remove redundant background color declaration

--- frontend/includes/footer.php
+++ frontend/includes/footer.php
@@ -2,7 +2,7 @@
 <footer>
     <a href="https://github.com/darks1g/ShootStars" target="_blank">
         <p>
-            <img src="../imgs/github-mark-white.svg" alt="github logo" /> <p>Ver repositorio en GitHub</p>
+            <img src="../imgs/github-mark-white.svg" alt="github logo" />
         </p>
     </a>
 </footer>
